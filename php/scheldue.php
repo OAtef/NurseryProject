2e8121bf1c55278620df11903e115d6c663682b6
Seif <seifelmosalamy@gmail.com>
2019-01-02 21:00:59 +0200

    scheldue

---
+++ php/scheldue.php
@@ -0,0 +1,136 @@
+<H1>My Timetable</H1>
+<TABLE class="myTimetable"> 
+  <THEAD>
+    <TR>
+      <TH></TH>
+      <TH>Monday</TH>
+      <TH>Tuesday</TH>
+      <TH>Wednesday</TH>
+      <TH>Thursday</TH>
+      <TH>Friday</TH>     
+    </TR>
+  </THEAD>
+  <TBODY>
+    <TR>
+      <TD>P1</TD>
+      <TD>
+        <div class="subject">Maths</div>
+        <div class="room">A120</div>
+      </TD>
+      <TD>
+        <div class="subject">Art</div>
+        <div class="room">C1</div>
+      </TD>
+      <TD>        
+        <div class="subject">English</div>
+        <div class="room">B21</div>
+      </TD>
+      <TD>        
+        <div class="subject">Maths</div>
+        <div class="room">A120</div>
+      </TD>
+       <TD>        
+         <div class="subject">Geography</div>
+        <div class="room">B101</div>
+      </TD>     
+    </TR>
+    <TR>
+      <TD>P2</TD>
+      <TD>        
+        <div class="subject">Science</div>
+        <div class="room">Lab1</div>
+      </TD>
+      <TD>        
+        <div class="subject">History</div>
+        <div class="room">B104</div>
+      </TD>
+      <TD>        
+        <div class="subject">Spanish</div>
+        <div class="room">C17</div>
+      </TD>
+      <TD>        
+        <div class="subject">P.E.</div>
+        <div class="room">A Gym</div>
+      </TD>
+      <TD>        
+        <div class="subject">Maths</div>
+        <div class="room">A120</div>
+      </TD>
+    </TR>
+      <TR>
+      <TD colspan="6" class="break">Break</TD>
+      </TR>
+      <TR>
+      <TD>P3</TD>
+      <TD>        
+        <div class="subject">I.T.</div>
+        <div class="room">ICT 1</div>
+      </TD>
+      <TD>        
+        <div class="subject">English</div>
+        <div class="room">B21</div>
+      </TD>
+      <TD>        
+        <div class="subject">Musique</div>
+        <div class="room">C5</div>
+      </TD>
+      <TD>        
+        <div class="subject">English</div>
+        <div class="room">B21</div>
+      </TD>
+      <TD>        
+        <div class="subject">PSHE</div>
+        <div class="room">A24</div>
+      </TD>
+    </TR>
+      <TR>
+      <TD colspan="6" class="lunch">Lunch</TD>
+    </TR>
+      <TR>
+      <TD>P4</TD>
+      <TD>        
+        <div class="subject">History</div>
+        <div class="room">B104</div>
+      </TD>
+      <TD>        
+        <div class="subject">Drama</div>
+        <div class="room">C17</div>
+      </TD>
+      <TD>        
+        <div class="subject">Maths</div>
+        <div class="room">A120</div>
+      </TD>
+      <TD>        
+        <div class="subject">Geography</div>
+        <div class="room">B101</div>
+      </TD>
+      <TD>        
+        <div class="subject">P.E.</div>
+        <div class="room">A Gym</div>
+      </TD>
+    </TR>
+      <TR>
+      <TD>P5</TD>
+      <TD>        
+        <div class="subject">Spanish</div>
+        <div class="room">C17</div>
+      </TD>
+      <TD>        
+        <div class="subject">Science</div>
+        <div class="room">Lab1</div>
+      </TD>
+      <TD>        
+        <div class="subject">English</div>
+        <div class="room">B21</div>
+      </TD>
+      <TD>        
+        <div class="subject">Science</div>
+        <div class="room">Lab1</div>
+      </TD>
+      <TD>        
+        <div class="subject">R.E.</div>
+        <div class="room">B18</div>
+      </TD>
+    </TR>
+   </TBODY>
+  </TABLE>
