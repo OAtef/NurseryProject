85d35b6fd94ed5e3189e3bdadc046ee27eef4427
Seif <seifelmosalamy@gmail.com>
2019-01-14 13:06:19 +0200

    Update ceo.php

--- php/CEO/ceo.php
+++ php/CEO/ceo.php
@@ -14,7 +14,7 @@ include('../SendMsg.php');
   <link rel="stylesheet" type="text/css" href="../../css/parent.css">
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
   <link rel="stylesheet" href="../../css/table.css">
-  <link rel="stylesheet" type="text/css" href="../css/Schedule.css">
+  <link rel="stylesheet" type="text/css" href="../../css/Schedule.css">
 </head>
 
 <body>
